dcc7c91481e839bae6de9d301d38755063c56db9
farida <dev18@laposte.net>
2024-12-15 11:20:50 +0100

    Добавьте файлы проекта.

---
+++ ImmiSwpper/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿window.registerInfiniteScroll = (dotNetHelper) => {
+    const content = document.getElementById("content");
+
+    content.addEventListener('scroll', function () {
+        if (content.scrollTop + content.clientHeight >= content.scrollHeight) {
+            dotNetHelper.invokeMethodAsync('LoadMoreContent');
+        }
+    });
+};
